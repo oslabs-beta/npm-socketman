583f372ce84c0c3b26a065ba2e0c50ae44ea272f
imz <duv.imma@gmail.com>
2022-10-11 10:30:53 -0400

    fix: bug on dist/test/server.js after running npm run build, have to manually switch the var setup = require("../index.ts to .js"

--- dist/test/server.js
+++ dist/test/server.js
@@ -4,42 +4,43 @@ exports.__esModule = true;
 var path = require("path");
 var app = require("express")();
 var http = require("http").Server(app);
-var setup = require("../index.ts");
+//after the build, had to convert from index.ts to .js
+var setup = require("../index.js");
 // would be best if the dev didn't have to manually allow CORS from our domain
 var io = require("socket.io")(http, {
-    cors: {
-        origin: "*"
-    }
+  cors: {
+    origin: "*",
+  },
 });
 var users = io.of("/users");
 var bongo = io.of("/bongo");
 var testnsp = io.of("/testnsp");
 setup(app, io);
 app.get("/", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "./test.html"));
+  res.sendFile(path.resolve(__dirname, "./test.html"));
 });
 io.on("connection", function (socket) {
-    // consider this middleware. this will catch all events and then continue through other "specific" listeners
-    socket.on("send message", function (msg) {
-        io.emit("receive message", msg);
-    });
-    socket.on("test-event", function (payload) { });
-    socket.on("change-color", function (array, callback) {
-        var color = [];
-        for (var i = 0; i < 3; i++) {
-            color.push(Math.floor(Math.random() * 256));
-        }
-        var colorStr = "rgb(".concat(color.join(", "), ")");
-        callback(colorStr);
-    });
-    socket.on("event-3", function () {
-        socket.emit("event-response", "hello client");
-    });
-    // console.log(socket.handshake);
-    // console.log(socket.rawListeners());
-    // console.log(socket.eventNames());
+  // consider this middleware. this will catch all events and then continue through other "specific" listeners
+  socket.on("send message", function (msg) {
+    io.emit("receive message", msg);
+  });
+  socket.on("test-event", function (payload) {});
+  socket.on("change-color", function (array, callback) {
+    var color = [];
+    for (var i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    var colorStr = "rgb(".concat(color.join(", "), ")");
+    callback(colorStr);
+  });
+  socket.on("event-3", function () {
+    socket.emit("event-response", "hello client");
+  });
+  // console.log(socket.handshake);
+  // console.log(socket.rawListeners());
+  // console.log(socket.eventNames());
 });
 http.listen(process.env.USERPORT || 1337, function () {
-    console.log("USER server running at 1337");
+  console.log("USER server running at 1337");
 });
 //# sourceMappingURL=server.js.map
